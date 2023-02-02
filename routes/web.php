cb4963d4b9391a1403e29e83cff8c5bbefc7f15b
Railton98 <railtonleal98@gmail.com>
2023-02-02 01:19:24 -0300

    chore: comment

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    \App\Jobs\Book\ConvertBookJob::dispatch('188e831e-0335-4bc9-9deb-c4d6dba6b258');
+    // \App\Jobs\Book\ConvertBookJob::dispatch('188e831e-0335-4bc9-9deb-c4d6dba6b258');
     return view('welcome');
 });
