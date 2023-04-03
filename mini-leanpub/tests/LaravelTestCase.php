9b672375110a1ce5cae974198743ec1988e87038
Railton98 <railtonleal98@gmail.com>
2023-04-02 23:15:19 -0300

    mod5: creating laravel testcase

---
+++ mini-leanpub/tests/LaravelTestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\MiniLeanpub;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LaravelTestCase extends TestCase
+{
+    use RefreshDatabase;
+}
